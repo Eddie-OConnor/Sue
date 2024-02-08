affaaa54ca3c786334b15f5d5a42a2643dcdb77e
Eddie-OConnor <eddie.oconnor3@gmail.com>
2024-02-08 14:25:55 -0500

    netlify serverless was timing out. reduced gpt complexity to avoid timeouts and having to upgrade to a paid plan

--- index.js
+++ index.js
@@ -9,19 +9,17 @@
 const ingredients = 'chicken breast, rice, green beans, tomato sauce, a full spice cabinet'
 const store = 'no'
 const people = '6'
-const leftovers = 'yes'
 const time = '3 hours'
-const allergies = 'gluten and dairy'
 const cookingEquipment = 'stove top, standard pots and pans, oven, crockpot'
 
-async function getRecipes(one, two, three, four, five, six, seven){
+async function getRecipes(one, two, three, four, five){
     try {
-        const response = await fetch('/.netlify/functions/fetchAI', {
+        const response = await fetch('/.netlify/functions/fetchAI', { 
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({one, two, three, four, five, six, seven})
+            body: JSON.stringify({one, two, three, four, five})
         })
         if(response.ok){
             const data = await response.json()
@@ -33,4 +31,4 @@ async function getRecipes(one, two, three, four, five, six, seven){
     }
 }
 
-getRecipes(ingredients, store, people, leftovers, time, allergies, cookingEquipment)
+// getRecipes(ingredients, store, people, time, cookingEquipment)
--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -2,8 +2,8 @@ import { openai } from '../../../config/openai.config'
 
 const handler = async (event) => {
     try {
-        const { one, two, three, four, five, six, seven } = JSON.parse(event.body)
-        const response = await generateRecipes(one, two, three, four, five, six, seven)
+        const { one, two, three, four, five } = JSON.parse(event.body)
+        const response = await generateRecipes(one, two, three, four, five)
         return {
             statusCode: 200,
             body: JSON.stringify({ response }),
@@ -14,25 +14,21 @@ const handler = async (event) => {
     }
 }
 
-async function generateRecipes(one, two, three, four, five, six, seven) {
+async function generateRecipes(one, two, three, four, five, six) {
     const messages = [
         {
             role: 'system',
-            content: `You are a master chef, recipe generator and virtual cookbook assistant. You provide 3-5 links to recipes 
-            from the web that adhere to the user submitted parameters of ${one}, ${two}, ${three}, ${four}, ${five}, ${six}, and ${seven}. Please also include 
-            a short 2-3 sentance description of the recipe so the user can get an idea of what the dish is before clicking the 
-            link to learn more and perhaps cook the dish.`
+            content: `You are a virtual cookbook assistant. Within 10 seconds, you provide 3 recipe URLs that adhere to the user submitted parameters of ${one}, ${two}, ${three}, ${four}, and ${five}`
         },
         {
             role: 'user',
-            content:`${one}, ${two}, ${three}, ${four}, ${five}, ${six}, and ${seven}`
+            content:`${one}, ${two}, ${three}, ${four}, and ${five}`
         }
     ]
     try {
         const response = await openai.chat.completions.create({
-            model: 'gpt-4',
+            model: 'gpt-3.5-turbo',
             messages: messages,
-            temperature: 1
         })
         console.log(response)
         return response
