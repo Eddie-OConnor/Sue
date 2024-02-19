cdf09a69fe6bd3a1deb328aca311e6bc24857934
Eddie-OConnor <eddie.oconnor3@gmail.com>
2024-02-19 15:12:47 -0500

    recipe result styling

--- uxFunctions.js
+++ uxFunctions.js
@@ -1,15 +1,3 @@
-// allergicDislike.addEventListener('change', function(event){
-//     const allergyDislikeInput = document.getElementById('allergy-dislike-input')
-//     const choice = event.target.value
-//     allergyDislikeInput.classList.toggle('hidden', event.target.value === 'No')
-
-//     if (choice === 'No'){
-//         document.getElementById('yes-radio-allergy-dislike').checked = false
-//     } else {
-//         document.getElementById('no-radio-allergy-dislike').checked = false
-//     }
-// })
-
 export function allergyDislikeChange(event){
     const allergyDislikeInput = document.getElementById('allergy-dislike-input')
     const choice = event.target.value
