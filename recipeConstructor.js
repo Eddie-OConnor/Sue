95b5de2b1261282029675dbc30c196fbddee01b8
Eddie-OConnor <eddie.oconnor3@gmail.com>
2024-02-20 09:58:59 -0500

    recipe constructor updates

--- recipeConstructor.js
+++ recipeConstructor.js
@@ -6,16 +6,16 @@ class Recipe {
     getRecipeHtml(){
         const {title, url, rating, time, ingredients, thumbnail, description} = this
         return `
-            <div class="recipe-container">
-                <a href="${url}"><img class="thumbnail" src="${thumbnail}" alt="recipe image thumbnail"></a>
-                <div class="recipe-details-container">
-                    <a href="${url}"><h3 class="recipe-title">${title}</h3></a>
-                    <span class="recipe-rating">${rating}</span>
-                    <span class="recipe-time">${time}</span>
-                    <span class="recipe-ingedients">${ingredients}</span>
+            <a href="${url}">
+                <div class="recipe-container">
+                    <img class="thumbnail" src="${thumbnail}" alt="recipe image thumbnail">
+                    <h3 class="recipe-title">${title}</h3>
+                    <p class="recipe-rating">${rating}</p>
+                    <p class="recipe-time">${time}</p>
+                    <p class="recipe-ingedients">${ingredients}</p>
                     <p class="recipe-description">${description}</p>
                 </div>
-            </div>
+            </a>
         `
     }
 }
